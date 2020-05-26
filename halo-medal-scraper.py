5c3ad90bb493aa24a84e47b93601f858cafaa574
AugmenTab <tbaum92@live.com>
2020-05-25 22:00:12 -0600

    Added to-do list.

--- halo-medal-scraper.py
+++ halo-medal-scraper.py
@@ -1,10 +1,21 @@
 
 
 
-games = [
-    'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals',
-    'https://www.halopedia.org/List_of_Halo:_Reach_Medals',
-    'https://www.halopedia.org/List_of_Halo_4_Medals',
-    'https://www.halopedia.org/List_of_Halo:_Spartan_Assault_Medals',
-    'https://www.halopedia.org/List_of_Halo:_Spartan_Strike_Medals'
+gameList = [
+    ('halo3', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'),
+    ('odst', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'),
+    ('reach', 'https://www.halopedia.org/List_of_Halo:_Reach_Medals'),
+    ('halo4', 'https://www.halopedia.org/List_of_Halo_4_Medals'),
+    ('spartanassault', 'https://www.halopedia.org/List_of_Halo:_Spartan_Assault_Medals'),
+    ('spartanstrike', 'https://www.halopedia.org/List_of_Halo:_Spartan_Strike_Medals')
 ]
+
+
+#TODO: Check if folder matching game name exists, make one if not.
+#TODO: Check if sheet on Excel file sheets, make one if not.
+#TODO: Range over table on URL page.
+#TODO: For each:
+    #TODO: Collect name and win condition, then append to Excel file.
+        # Make sure to ignore section headers.
+    #TODO: Navigate to image page, download image, and record file name.
+    #TODO: Add name to correct line on Excel file.
