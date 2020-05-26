ed701226e5cc9510d244d09f52329984ce699440
AugmenTab <tbaum92@live.com>
2020-05-25 21:31:53 -0600

    Added URLs for each game's medal list.

---
+++ halo-medal-scraper.py
@@ -0,0 +1,10 @@
+
+
+
+games = [
+    'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals',
+    'https://www.halopedia.org/List_of_Halo:_Reach_Medals',
+    'https://www.halopedia.org/List_of_Halo_4_Medals',
+    'https://www.halopedia.org/List_of_Halo:_Spartan_Assault_Medals',
+    'https://www.halopedia.org/List_of_Halo:_Spartan_Strike_Medals'
+]
