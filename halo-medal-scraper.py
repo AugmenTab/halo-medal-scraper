103ea83f5c3937e6d81a93c43f668a18bd33cd8a
AugmenTab <tbaum92@live.com>
2020-05-26 20:37:54 -0600

    Fixed list, added openpyxl module.

--- halo-medal-scraper.py
+++ halo-medal-scraper.py
@@ -1,20 +1,20 @@
-
+import openpyxl
 
 
 gameList = [
-    ('halo3', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'),
-    ('odst', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'),
-    ('reach', 'https://www.halopedia.org/List_of_Halo:_Reach_Medals'),
-    ('halo4', 'https://www.halopedia.org/List_of_Halo_4_Medals'),
-    ('spartanassault', 'https://www.halopedia.org/List_of_Halo:_Spartan_Assault_Medals'),
-    ('spartanstrike', 'https://www.halopedia.org/List_of_Halo:_Spartan_Strike_Medals')
+    ['halo3', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'],
+    ['odst', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'],
+    ['reach', 'https://www.halopedia.org/List_of_Halo:_Reach_Medals'],
+    ['halo4', 'https://www.halopedia.org/List_of_Halo_4_Medals'],
+    ['spartan-assault', 'https://www.halopedia.org/List_of_Halo:_Spartan_Assault_Medals'],
+    ['spartan-strike', 'https://www.halopedia.org/List_of_Halo:_Spartan_Strike_Medals']
 ]
 
 
 #TODO: Check if folder matching game name exists, make one if not.
-#TODO: Check if sheet on Excel file sheets, make one if not.
+#TODO: Check if sheet matching game name exist on Excel file, make one if not.
 #TODO: Range over table on URL page.
-#TODO: For each:
+#TODO: For each table row:
     #TODO: Collect name and win condition, then append to Excel file.
         # Make sure to ignore section headers.
     #TODO: Navigate to image page, download image, and record file name.
