a551480bb72cdd5d3a3aea3bc0354c8795b315fd
AugmenTab <tbaum92@live.com>
2020-05-27 21:45:40 -0600

    Reorganized game list, made folder function.

--- halo-medal-scraper.py
+++ halo-medal-scraper.py
@@ -1,21 +1,66 @@
-import openpyxl
+#! python3
+
+import os #, openpyxl
+#wb = openpyxl.load_workbook('medal-list.xlsx')
 
 
 gameList = [
-    ['halo3', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'],
-    ['odst', 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'],
-    ['reach', 'https://www.halopedia.org/List_of_Halo:_Reach_Medals'],
-    ['halo4', 'https://www.halopedia.org/List_of_Halo_4_Medals'],
-    ['spartan-assault', 'https://www.halopedia.org/List_of_Halo:_Spartan_Assault_Medals'],
-    ['spartan-strike', 'https://www.halopedia.org/List_of_Halo:_Spartan_Strike_Medals']
+    {
+        'game': 'halo3', 
+        'url': 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'
+    },
+    {
+        'game': 'odst', 
+        'url': 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'},
+    {
+        'game': 'reach', 
+        'url': 'https://www.halopedia.org/List_of_Halo:_Reach_Medals'
+    },
+    {
+        'game': 'halo4', 
+        'url': 'https://www.halopedia.org/List_of_Halo_4_Medals'
+    },
+    {
+        'game': 'spartan-assault', 
+        'url': 'https://www.halopedia.org/List_of_Halo:_Spartan_Assault_Medals'
+    },
+    {
+        'game': 'spartan-strike', 
+        'url': 'https://www.halopedia.org/List_of_Halo:_Spartan_Strike_Medals'
+    }
 ]
 
+test = [
+    {
+        'game': 'halo3', 
+        'url': 'https://www.halopedia.org/List_of_Halo_3_and_Halo_3:_ODST_Medals'
+    }
+]
 
-#TODO: Check if folder matching game name exists, make one if not.
+def makeFolder(gameName):
+    dir = os.path.join('.', 'medals', str(gameName))
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    
+
 #TODO: Check if sheet matching game name exist on Excel file, make one if not.
+def makeSheets(gameName):
+    print(gameName)
+
 #TODO: Range over table on URL page.
+
+
 #TODO: For each table row:
     #TODO: Collect name and win condition, then append to Excel file.
         # Make sure to ignore section headers.
     #TODO: Navigate to image page, download image, and record file name.
     #TODO: Add name to correct line on Excel file.
+
+
+def scrapeMedals(games):
+    for game in games:
+        #makeFolder(game['game'])
+        makeSheets(game['game'])
+
+if __name__ == "__main__":
+    scrapeMedals(test) #switch to gameList when complete
